5f1a917c8180c8465df0091646f2d47a78fe9709
Abel Cunha <abel.cunha@kcsit.pt>
2024-10-21 12:20:28 +0100

    Add project files.

---
+++ AOCMAUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AOCMAUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AOCMAUIApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AOCMAUIApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
